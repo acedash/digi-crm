84e934f5cea2170762a0131f2a6a874d9f7eba59
acedash <mailtokhanasrar@gmail.com>
2026-04-09 00:00:27 +0530

    fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../features/auth/useAuthStore';
 
 export const BACKEND_BASE_URL = 'https://lightyellow-vulture-726958.hostingersite.com/api';
-export const API_BASE_URL = '/api';
+export const API_BASE_URL = `${BACKEND_BASE_URL}/api`;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
