cca25d78923e839c9742fd6eb9724ac007641bb9
acedash <mailtokhanasrar@gmail.com>
2026-04-08 23:04:43 +0530

    addedn new buid

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../features/auth/useAuthStore';
 
-export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'https://lightyellow-vulture-726958.hostingersite.com/api/';
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'https://lightyellow-vulture-726958.hostingersite.com/api';
 export const API_BASE_URL = import.meta.env.VITE_API_URL || `${BACKEND_BASE_URL}/api`;
 
 const api = axios.create({
