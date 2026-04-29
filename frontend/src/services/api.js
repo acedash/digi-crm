72719a976796cadee2d01be81cf602aa269b6ec4
acedash <mailtokhanasrar@gmail.com>
2026-04-29 21:29:05 +0530

    added new

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { useAuthStore } from '../features/auth/useAuthStore';
-export const BACKEND_BASE_URL = import.meta.env.DEV 
-    ? 'http://127.0.0.1:8001' 
-    : 'https://lightyellow-vulture-726958.hostingersite.com';
+export const BACKEND_BASE_URL = import.meta.env.DEV
+    ? 'http://127.0.0.1:8001'
+    : 'https://crm.thedigicircle.com';
 export const API_BASE_URL = `${BACKEND_BASE_URL}/api`;
 
 const api = axios.create({
